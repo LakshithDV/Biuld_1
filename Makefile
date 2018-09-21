929ed4e8edac261ee92a6eca88e6e2ff09bc0a92
root <root@ip-172-31-26-184.us-east-2.compute.internal>
2018-09-21 18:59:05 +0000

    done with abc.exe

--- Makefile
+++ Makefile
@@ -1,1 +1,12 @@
-
+abc.exe:add2n.o evod.o mul2.c make.c
+	gcc -o abc.exe add2n.o evod.o mul2.c make.c	
+add2n.o:add2n.c
+	gcc -c add2n.c
+evod.o:evod.c
+	gcc -c evod.c
+mul2.o:mul2.c
+	gcc -c mul2.c
+make.o:make.c
+	gcc -c make.c	
+Clean:
+	rm -rf *.o
--- evod.c
+++ evod.c
@@ -4,9 +4,9 @@ int c2()
     int n=4;
 
     // True if the number is perfectly divisible by 2
-    if(number % 2 == 0)
-        printf("%d is even.", number);
+    if(n % 2 == 0)
+        printf("%d is even.", n);
     else
-        printf("%d is odd.", number);
+        printf("%d is odd.", n);
 
 }
